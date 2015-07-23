1d5fa0df5c031762e68277711c654af77678873c
mcgml <mcgml@cardiff.ac.uk>
2015-07-23 09:45:16 +0100

    initial commit

--- Main.java
+++ Main.java
@@ -1,8 +1,35 @@
 package nhs.genetics.cardiff;
 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
+    private static final double version = 0.1;
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-	// write your code here
+
+        if (args.length != 2) {
+            System.err.println("Usage: <SampleSheet.csv> <runParameters.xml>");
+            System.exit(1);
+        }
+
+        log.log(Level.INFO, "Make variable files v" + version);
+
+        //parse samplesheet
+        IlluminaSampleSheetFile illuminaSampleSheetFile = new IlluminaSampleSheetFile(new File(args[0]));
+        illuminaSampleSheetFile.parseSampleSheet();
+        illuminaSampleSheetFile.populateSampleSheetValues();
+
+        //parse run-parameters
+        IlluminaRunParametersFile illuminaRunParametersFile = new IlluminaRunParametersFile(new File (args[1]));
+        illuminaRunParametersFile.parseRunParametersXml();
+
+        //make pipeline config files
+        PipelineConfigFileMaker.makeIlluminaConfigFile(illuminaSampleSheetFile, illuminaRunParametersFile);
+
+
     }
 }
