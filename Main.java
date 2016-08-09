fe1258cd3b5808131677848b0ed86efde231841e
mcgml <mcgml@cardiff.ac.uk>
2016-08-09 16:17:55 +0100

    updated to v2

--- Main.java
+++ Main.java
@@ -1,34 +1,46 @@
 package nhs.genetics.cardiff;
 
+import nhs.genetics.cardiff.framework.IlluminaRunParametersFile;
+import nhs.genetics.cardiff.framework.IlluminaSampleSheetFile;
+import nhs.genetics.cardiff.framework.PipelineConfigFileMaker;
+
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
 
-    private static final double version = 0.1;
+    private static final String version = "2.0.0";
     private static final Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
         if (args.length != 2) {
-            System.err.println("Usage: <SampleSheet.csv> <runParameters.xml>");
+            System.err.println("Usage: <SampleSheet.csv> <RunParameters.xml>");
             System.exit(1);
         }
 
         log.log(Level.INFO, "Make variable files v" + version);
 
-        //parse samplesheet
         IlluminaSampleSheetFile illuminaSampleSheetFile = new IlluminaSampleSheetFile(new File(args[0]));
-        illuminaSampleSheetFile.parseSampleSheet();
-        illuminaSampleSheetFile.populateSampleSheetValues();
-
-        //parse run-parameters
         IlluminaRunParametersFile illuminaRunParametersFile = new IlluminaRunParametersFile(new File (args[1]));
-        illuminaRunParametersFile.parseRunParametersXml();
 
-        //make pipeline config files
-        PipelineConfigFileMaker.makeIlluminaConfigFile(illuminaSampleSheetFile, illuminaRunParametersFile);
+        try {
+
+            //parse samplesheet
+            illuminaSampleSheetFile.parseSampleSheet();
+            illuminaSampleSheetFile.populateSampleSheetValues();
+
+            //parse run-parameters
+            illuminaRunParametersFile.parseRunParametersXml();
+
+            //make pipeline config files
+            PipelineConfigFileMaker.makeIlluminaConfigFile(illuminaSampleSheetFile, illuminaRunParametersFile);
+
+        } catch (Exception e){
+            log.log(Level.SEVERE, e.getMessage());
+            System.exit(-1);
+        }
 
     }
 }
